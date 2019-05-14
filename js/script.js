97c371f910f9715523cc6ae69dda564868e6849e
martijndeheer <martijndeheer@gmail.com>
2019-05-14 15:52:10 +0200

    using other data that is more representable

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function setup() {
     let thisCanvas = createCanvas(600, 400);
     thisCanvas.parent('this-canvas');
 
-    openset_piechart = new pieChart(openset_data, "open set");
+    openset_piechart = new pieChart(open_data, "open set");
 }
 
 function draw() {
